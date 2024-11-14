2ac4505220853674c04aa6c03a47284f2ce1e139
Samuel Agyekum <agyekumsamuel055@gmail.com>
2024-11-14 19:59:36 +0000

    Add changes to urls files

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -3,11 +3,24 @@ from . import views
 
 urlpatterns = [
     path('booklist/',views.booklist, name = "booklist"),
+
     path('librarylist/<int:pk>/',views.LibraryListView.as_view(), name ="librarylist"),
+
     path('adminsonly/', views.AdminOnlyView, name = "adminsonly"),
+
     path('librarian/', views.LibrarianView, name = "librarian"),
+
     path('login/', views.login.as_view(), name = 'login'),
+
     path('logout/',views.logout.as_view(), name = 'logout'),
+
     path('register/',views.register.as_view(), name = 'register'),
-    path('profile/', views.ProfileView.as_view(), name='profile')
+
+    path('profile/', views.ProfileView.as_view(), name='profile'),
+
+     path('add_book/', views.add_book, name='add_book'),   
+                     # URL for adding a book
+    path('edit_book/<int:book_id>/', views.edit_book, name='edit_book'), 
+    
+    path('delete_book/<int:book_id>/', views.delete_book, name='delete_book'),
 ]
