fb8db5f022df157ea4904107f5bd16d63990e8a3
Samuelghostv <agyekumsamuel055@gmail.com>
2024-11-17 07:55:43 +0000

    Add class for customuser admin in admin file update

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -3,11 +3,15 @@ from .models import Author, Book
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 from .import admin_site, CustomUserAdmin
+from .forms import CustomUserCreationForm, CustomUserChangeForm 
 
 
 
 # Integrate the user admin with our user model
 class CustomUserAdmin(UserAdmin):
+
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
     model = CustomUser
     list_display =["username", "email", "date_of_birth", "profile_photo", "is_admin"]
 
