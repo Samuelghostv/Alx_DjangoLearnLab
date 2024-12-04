66806ed2fec23669e5199f071285039dba4c439f
Samuelghostv <agyekumsamuel055@gmail.com>
2024-12-04 13:05:57 +0000

    Add first endpoint with django framework

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import BookSerializer
+from .models import Book
 
 # Create your views here.
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()     # this is will fetch or retrieve all the book objects
+    serializer_class = BookSerializer # this is will format the data of the BookSerializer
