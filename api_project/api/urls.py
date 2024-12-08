e606dd039cb10d9b6c47bad4e7b44a6da2d58ec0
Samuelghostv <agyekumsamuel055@gmail.com>
2024-12-08 17:00:40 +0000

    Add authentication and permissions for DRF

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from .views import BookList
 from rest_framework.routers import DefaultRouter
 from .views import BookViewSet
+from rest_framework.authtoken.views import obtain_auth_token
 
 # create the router and register our BookViewSets with it
 router = DefaultRouter()
@@ -10,6 +11,7 @@ router.register(r'books_all', BookViewSet, basename = 'books_all')
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
     path('', include(router.urls)),
+    path('api-token-auth/', name = 'obtain_auth_token'),
 ]
 
 # DefaultRouter: this will router automatically generates the necessary URL patterns for all CRUD operations
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,6 +3,19 @@ from rest_framework import generics
 from .serializers import BookSerializer
 from .models import Book
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+# to create IsAuthenticated permission class
+class MyViewSet(APIView):
+    permission_class = [IsAuthenticated]
+
+    def get(self, request):
+        return Response({"message": "You are authenticated User!"}, status=status.HTTP_200_OK)
+#IsAuthenticated: ensures that only authenticated users can access the view
+
 
 # Create your views here.
 
