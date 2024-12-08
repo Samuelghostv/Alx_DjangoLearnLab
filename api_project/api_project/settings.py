7cf2f0d183461421b63ff571f7c65b552c1b38f4
Samuelghostv <agyekumsamuel055@gmail.com>
2024-12-08 19:42:37 +0000

    Add authentication and permissions for DRF

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -46,7 +46,13 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
-    ]
+    ],
+
+    'DEFAULT_PERMISSION_CLASSES' : [
+        'rest_framework.permissions.IsAuthenticated', 
+        # this is the default permissions class is be IsAuthenticated
+    ],
+  
 }
 
 MIDDLEWARE = [
