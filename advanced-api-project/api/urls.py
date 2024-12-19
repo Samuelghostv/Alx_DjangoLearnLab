22bcb612ea47d3d35927f84bb719d504d6789cc2
Samuelghostv <agyekumsamuel055@gmail.com>
2024-12-19 23:16:09 +0000

    Add custom views and generics views in the DRF

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -5,9 +5,9 @@ from .views import BookListView, BookDetailView, BookCreateView, BookUpdateView,
 
 urlpatterns = [
     path("authors/", AuthorListView.as_view(), name="author-list"),
-    path("books/", BookListView.as_view(), name="book-list"),
-    path("books/<int:pk>/", BookDetailView.as_view(), name="book-detail"),
-    path("books/create/", BookCreateView.as_view(), name="book-create"),
-    path("books/<int:pk>/update/", BookUpdateView.as_view(), name="book-update"),
-    path("books/<int:pk>/delete/", BookDeleteView.as_view(), name="book-delete"),
+     path("books/", BookListView.as_view(), name="book-list"),  # List all books
+    path("books/<int:pk>/", BookDetailView.as_view(), name="book-detail"),  # Retrieve a single book
+    path("books/create/", BookCreateView.as_view(), name="book-create"),  # Create a new book
+    path("books/<int:pk>/update/", BookUpdateView.as_view(), name="book-update"),  # Update a book
+    path("books/<int:pk>/delete/", BookDeleteView.as_view(), name="book-delete"),  # Delete a book
 ]
