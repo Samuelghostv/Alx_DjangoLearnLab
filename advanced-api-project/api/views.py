b3f83caf782a5212fa392781d1f26bf7f78b8550
Samuelghostv <agyekumsamuel055@gmail.com>
2024-12-20 00:34:07 +0000

    Make changes ti views file

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framwork.views import APIView
 from rest_framework.response import Response
 from .models import Author
@@ -6,7 +7,6 @@ from .serializers import AuthorSerializer
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 
 # Create your views here.
 # List all the books
