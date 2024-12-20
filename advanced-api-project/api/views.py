aaeeb39f847a3f388f7bd6ce7fc00fb9bf87be0e
Samuelghostv <agyekumsamuel055@gmail.com>
2024-12-20 11:45:45 +0000

    Add some changes to the views file

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,4 +1,5 @@
 "from django_filters import rest_framework"
+from .filters import OrderingFilter
 "from rest_framework.filters import OrderingFilter"
 "from rest_framework.filters import SearchFilter"
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
