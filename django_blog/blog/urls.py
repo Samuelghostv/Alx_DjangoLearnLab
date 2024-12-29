dbb5d690a656dd3f729828069d2245ce9dfabb44
Samuelghostv <agyekumsamuel055@gmail.com>
2024-12-29 23:34:19 +0000

    Add little changes to the views file

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -4,11 +4,11 @@ from . import views
 from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 
 urlpatterns = [
-    path('', PostListView.as_view(), name= "post_list"),
-    path("posts/<int:pk>/", PostDetailView.as_view(), name= "post_detail"),
-    path("posts/new/", PostCreateView.as_view(), name= "post_create"),
-    path("posts/<int:pk>/edit/", PostUpdateView.as_view(), name= "post_edit"),
-    path("posts/<int:pk>/delete/", PostDeleteView.as_view(), name= "post_delete"),
+    path("", PostListView.as_view(), name= "post_list"),
+    path("post/<int:pk>/", PostDetailView.as_view(), name= "post_detail"),
+    path("post/new/", PostCreateView.as_view(), name= "post_create"),
+    path("post/<int:pk>/edit/", PostUpdateView.as_view(), name= "post_edit"),
+    path("post/<int:pk>/delete/", PostDeleteView.as_view(), name= "post_delete"),
     path('login/', views.user_login, name="login"),
     path('logout/', views.user_logout, name="logout"),
     path('register/', views.register, name="register"),
